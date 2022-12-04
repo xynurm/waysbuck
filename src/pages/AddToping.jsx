f89c46c4923b9636b26402cf1fd69c0c39a3afd1
xynurm <nyakridha24@gmail.com>
2022-12-05 05:07:09 +0700

    add toping

--- src/pages/AddToping.jsx
+++ src/pages/AddToping.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import {
-  Button, Col,
+  Button,
+  Col,
   Container,
+  Form,
   FormControl,
   FormGroup,
   Row
 } from "react-bootstrap";
+import { useMutation } from "react-query";
+import { API } from "../config/api";
 const Text = {
   Red: {
     color: "#BD0707"
@@ -30,82 +34,95 @@ const CustomBtn = {
   border: "none"
 };
 export default function AddToping() {
-  const topings = []
+  const [preview, setPreview] = useState(null); //For image preview
+  const [form, setForm] = useState({
+    title: "",
+    price: "",
+    image: ""
+  });
 
-
-  const[toping, setToping] = useState({
-    id:0,
-    name:"",
-    topPrice:0,
-    img:""
-  })
-
-  const createToping = () => {
-    let dataToping = JSON.parse(localStorage.getItem("DATA_TOPING"))
-    if(dataToping !== null){
-      dataToping.forEach(element => {
-        topings.push(element)
-      })
-      toping.id = topings.length;
-      topings.push(toping)
-      localStorage.setItem("DATA_TOPING", JSON.stringify(topings))
-    }else{
-      toping.id = topings.length;
-      topings.push(toping)
-      localStorage.setItem("DATA_TOPING", JSON.stringify(topings))
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]:
+        e.target.type === "file" ? e.target.files : e.target.value
+    });
+    if (e.target.type === "file") {
+      let url = URL.createObjectURL(e.target.files[0]);
+      setPreview(url);
     }
-  }
+  };
 
-  const handleOnChange = (e) => {
-    setToping({
-      ...toping,
-      [e.target.name] : e.target.value
-    })
-  }
+  const handleSubmit = useMutation(async (e) => {
+    try {
+      e.preventDefault();
+      const formData = new FormData();
+      formData.set("image", form.image[0], form.image[0].name);
+      formData.set("title", form.title);
+      formData.set("price", form.price);
+      const response = await API.post("/toping", formData);
+      console.log("data porduct berhasil ditambahkan", response.data.data);
+    } catch (err) {
+      console.log(err);
 
-  const handlePrice = (e) => {
-    setToping({
-      ...toping,
-      topPrice: e.target.valueAsNumber
-    })
-  }
-
-  const handleOnSubmit = (e) => {
-    window.location.reload()
-    createToping()
-    
-  }
-
-// urlImage
-  // https://www.linkpicture.com/q/top1.png - top8
+      alert("failed add product");
+    }
+  });
 
   return (
     <Container>
       <Row>
         <Col className="pt-5 px-5">
           <div className="pb-5">
-            <h4 style={Text.Red} className="fw-bold">Toping</h4>
+            <h4 style={Text.Red} className="fw-bold">
+              Toping
+            </h4>
           </div>
-          <FormGroup className="mb-4">
-            <FormControl placeholder="Name Toping" type="text" name="name" onChange={handleOnChange} style={Input}></FormControl>
-          </FormGroup>
-          <FormGroup className="mb-4">
-            <FormControl placeholder="Price" type="number" name="topPrice" onChange={handlePrice} style={Input}></FormControl>
-          </FormGroup>
-          <FormGroup className="mb-4">
-            <FormControl type="file" id="upload" hidden />
-            <FormControl placeholder="Url Image" type="text" name="img" onChange={handleOnChange} style={Input}></FormControl>
-          </FormGroup>
-          <div className="d-grid gap-2 mb-3">
-            <Button style={CustomBtn} onClick={handleOnSubmit} className="fw-semibold">
-              Add Toping
-            </Button>
-          </div>
+          <Form onSubmit={(e) => handleSubmit.mutate(e)}>
+            <FormGroup className="mb-4">
+              <FormControl
+                placeholder="Name Toping"
+                type="text"
+                name="title"
+                onChange={handleChange}
+                style={Input}
+              ></FormControl>
+            </FormGroup>
+            <FormGroup className="mb-4">
+              <FormControl
+                placeholder="Price"
+                type="number"
+                name="price"
+                onChange={handleChange}
+                style={Input}
+              ></FormControl>
+            </FormGroup>
+            <FormGroup className="mb-4">
+              <FormControl
+                type="file"
+                name="image"
+                onChange={handleChange}
+                style={Input}
+              ></FormControl>
+            </FormGroup>
+            <div className="d-grid gap-2 mb-3">
+              <Button
+                style={CustomBtn}
+                onClick={(e) => handleSubmit.mutate(e)}
+                className="fw-semibold"
+              >
+                Add Toping
+              </Button>
+            </div>
+          </Form>
         </Col>
         <Col sm={5} className="pt-5 px-5">
-      
-       <img src={toping.img}  className="rounded-4" alt="product-img"/>
-      </Col>
+          {preview && (
+            <div>
+              <img src={preview} alt={preview} />
+            </div>
+          )}
+        </Col>
       </Row>
     </Container>
   );
