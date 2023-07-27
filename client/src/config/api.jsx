af0593981c554e6cbcddb39f29d860a3399edc7d
xynurm <nyakridha24@gmail.com>
2023-07-27 09:14:40 +0700

    first commit

--- client/src/config/api.jsx
+++ client/src/config/api.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const API = axios.create({
-    // baseURL: process.env.REACT_APP_BASEURL,
-    baseURL: 'http://localhost:5000/api/v1'
+    // baseURL: process.env.REACT_APP_BASEURL, Production
+    baseURL: 'http://localhost:5000/api/v1' //local
 })
 
 export const setAuthToken = (token) => {
